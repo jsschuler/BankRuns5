102aa1699c522b51ec5e8ca1e4e83bd0c26a31f9
jsschuler <jsschuler@gmail.com>
2022-12-23 15:40:47 -0500

    https://github.com/jsschuler/BankRuns

---
+++ model0001.jl
@@ -0,0 +1,8 @@
+# the core idea of this version of the bank run model is that we burn it in
+# meaning agents decide whether they want to keep banking before the exogenous
+# withdrawals begin
+
+# also though, we want to avoid the overhead of recompiling.
+# so, as much of the work as possible happens inside Julia
+
+# also, ideally, boosting methods can help with the search of parameter spaces.
