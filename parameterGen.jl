78e4b251d1ec246f66e4177aa7a0aa540f698332
jsschuler <jsschuler@gmail.com>
2025-10-25 17:46:05 -0400

    modified:   parameterGen.jl

--- parameterGen.jl
+++ parameterGen.jl
@@ -33,7 +33,7 @@
 # and a vector of the needed parameters for each
 # GRAPHS for each graph, a and b in (0,1)
 
-genSeed=9673674
+genSeed=3248473
 Random.seed!(genSeed)
 
 # how many model initializations to run?
